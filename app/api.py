dfd4b5479d61ee099aa38a8dd03ab365d8bd470f
hector3910 <152331496+hector3910@users.noreply.github.com>
2025-10-16 20:45:24 -0500

    Proyecto completo con API, Docker, Kubernetes y CI

---
+++ app/api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+import pandas as pd
+
+model = joblib.load("app/model.joblib")
+app = FastAPI()
+
+class Input(BaseModel):
+    features: list
+
+@app.post("/predict")
+def predict(data: Input):
+    columns = ['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol',
+               'FastingBS', 'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope']
+    X = pd.DataFrame([data.features], columns=columns)   
+    proba = model.predict_proba(X)[0][1]
+    return {"heart_disease_probability": proba, "prediction": int(proba > 0.5)}
